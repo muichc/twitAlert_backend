9232fe03d1298561dfe38b0e32a6a0c1291d823a
muichc <cindy.ch.mui@gmail.com>
2021-04-06 21:40:46 -0700

    trying things with user

--- twitalertapp/main.py
+++ twitalertapp/main.py
@@ -1,24 +1,5 @@
-from flask import Flask, send_from_directory, jsonify, Blueprint
+from flask import Flask, send_from_directory, jsonify, Blueprint, request
 from.extensions import mongo
-
-# from flask_mongoengine import MongoEngine
-# from flask_pymongo import PyMongo
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
-
-# app = Flask(__name__, static_folder='static', static_url_path='')
-# DB_URI = os.getenv('MONGODB_URI')
-# # app.config['MONGODB_HOST'] = DB_URI
-# # cluster = MongoClient(DB_URI)
-# # db = cluster["test"]
-# # collection = db["test"]
-# app.config["MONGO_URI"] = DB_URI
-# mongo = PyMongo(app)
-
-# post = {"_id": 0, "name": "tim", "score":5}
-
-# mongo.insert_one(post)
 
 main = Blueprint('main', __name__)
 
@@ -29,5 +10,5 @@ def index():
     user_collection.insert({'name':'Anthony'})
     return '<h1>Added a User! </h1>'
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
---
+++ twitalertapp/models/user.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+
+# class User:
+
+#     def signup(self):
+
+#         user = {
+#             "_id"
+#         }
